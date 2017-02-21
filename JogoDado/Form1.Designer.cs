3d19bd4fe52f85ae200c1691e5acd8f4e47580a3
maikrodrigues <maik_rr@hotmail.com>
2017-02-20 22:47:25 -0300

    Add project files.

---
+++ JogoDado/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace JogoDado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.bt3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(36, 46);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(75, 23);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "Jogador 1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(179, 46);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(75, 23);
+            this.bt2.TabIndex = 1;
+            this.bt2.Text = "Jogador 2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(21, 108);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(88, 13);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Primeira Rodada:";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(21, 134);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(94, 13);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "Segunda Rodada:";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(21, 161);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(90, 13);
+            this.lbl3.TabIndex = 4;
+            this.lbl3.Text = "Terceira Rodada:";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(131, 105);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 5;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(131, 131);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 6;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(131, 158);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 7;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(131, 208);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 20);
+            this.txt4.TabIndex = 8;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(35, 20);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(103, 13);
+            this.lbl4.TabIndex = 9;
+            this.lbl4.Text = "Jogador 1 é sua vez";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::JogoDado.Properties.Resources.Gifs_animados_Dado_8;
+            this.pictureBox1.Location = new System.Drawing.Point(363, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(268, 110);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(0, 13);
+            this.lbl5.TabIndex = 11;
+            // 
+            // bt3
+            // 
+            this.bt3.Location = new System.Drawing.Point(335, 46);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(75, 23);
+            this.bt3.TabIndex = 12;
+            this.bt3.Text = "Verificar";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 279);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Button bt3;
+    }
+}
+
---
+++ JogoDado/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDado
+{
+    public partial class Form1 : Form
+    {
+        int v1, v2;
+        bool jogador1 = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bt1.Click += new EventHandler(BClick);
+            bt2.Click += new EventHandler(BClick);
+            pictureBox1.Visible = false;
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.TabStop = false;
+                }
+            }
+        }
+
+        private void BClick(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.jogador1 ? "Jogador 2" : "Jogador 1";
+            ((Button)sender).Enabled = false;
+            jogador1 = !jogador1;
+            lbl4.Text = string.Format("{0} é sua vez", jogador1 ? "Jogador 1" : "Jogador 2");
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            for (int i = 0; i < 3; i++)
+            {
+                Random rnd = new Random();
+                int valor = rnd.Next(1, 7);
+                lbl5.Text = Convert.ToString(valor);
+                v1 = valor;
+                MessageBox.Show("Sorteado");
+            }
+            pictureBox1.Visible = false;
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            for (int i = 0; i < 3; i++)
+            {
+                Random rnd = new Random();
+                int valor = rnd.Next(1, 7);
+                lbl5.Text = Convert.ToString(valor);
+                v2 = valor;
+                MessageBox.Show("Sorteado");
+                if (v1 > v2)
+                {
+                    txt1.Text = "Jogador 1";
+                }
+                else
+                    txt1.Text = "Jogador 2";
+                if (v1 == v2)
+                {
+                    txt1.Text = "Empate";
+                }
+                if (v1 > v2)
+                {
+                    txt2.Text = "Jogador 1";
+                }
+                else
+                    txt2.Text = "Jogador 2";
+                if (v1 == v2)
+                {
+                    txt2.Text = "Empate";
+                }
+                if (v1 > v2)
+                {
+                    txt3.Text = "Jogador 1";
+                }
+                else
+                    txt3.Text = "Jogador 2";
+                if (v1 == v2)
+                {
+                    txt3.Text = "Empate";
+                }
+            }
+            pictureBox1.Visible = false;
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
